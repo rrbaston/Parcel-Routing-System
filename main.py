613e2bde7b85ce1a46ff6fc23691e12705492eb4
Rodrigo Baston <rodrigo.rbaston@gmail.com>
2023-12-18 19:13:53 -0800

    Phase A complete

--- main.py
+++ main.py
@@ -7,28 +7,28 @@ def loadPackageData(hash_table, file_path):
         with open(file_path, mode='r') as file:
             csv_reader = csv.reader(file)
             next(csv_reader)  # Skip the header row
+            next(csv_reader)  # Skip the header row
             for row in csv_reader:
                 # Assuming the CSV columns are in the order:
                 # PackageID, Address, Deadline, City, Zip Code, Weight, Status
-                package_id = row[0]
+                package_id = int(row[0])
                 address = row[1]
                 city = row[2]
-                zip_code = row[3]
-                deadline = row[4]
-                weight = row[5]
-                status = row[6]
+                state = row[3]
+                zip = row[4]
+                deadline = row[5]
+                weight = row[6]
+                status = row[7]
                 delivery_time = None  # No delivery time in csv
 
                 # Create a Package object
-                package = Package(package_id, address, city, zip_code, deadline, weight, status, delivery_time)
+                package = Package(package_id, address, city, state, zip, deadline, weight, status, delivery_time)
 
                 # Insert the Package object into the HashTable
                 hash_table.insert(package)
 
-
-
 hash_table = HashTable(size=10)  # Create a HashTable instance
-loadPackageData(hash_table, '/Users/rodrigo/Documents/repos/Parcel-Routing-System/WGUPS-address.csv')
-
+loadPackageData(hash_table, '/Users/rodrigo/Documents/repos/Parcel-Routing-System/WGUPS-package-v2.csv')
+hash_table.display()
 
 
--- package.py
+++ package.py
@@ -1,16 +1,17 @@
 class Package:
-    def __init__(self, package_id, address, city, zip_code, deadline, weight, status, delivery_time = None):
+    def __init__(self, package_id, address, city, state, zip, deadline, weight, status, delivery_time = None):
         self.package_id = package_id
         self.address = address
         self.city = city
-        self.zip_code = zip_code
+        self.state = state
+        self.zip = zip
         self.deadline = deadline
         self.weight = weight
-        if status not in ["at the hub", "en route", "delivered"]:
-            raise ValueError("Invalid status. Status must be 'at the hub', 'en route', or 'delivered'.")
+#        if status not in ["at the hub", "en route", "delivered"]:
+#            raise ValueError("Invalid status. Status must be 'at the hub', 'en route', or 'delivered'.")
         self.status = status
         self.delivery_time = delivery_time if status == "delivered" else None
 
     def __repr__(self):
-        return (f"{self.package_id}, {self.address}, {self.deadline}, {self.city}, "
-                f"{self.zip_code}, {self.weight}, {self.status}" + (f", {self.delivery_time}" if self.delivery_time else "") + ")")
+        return (f"{self.package_id}, {self.address}, {self.city}, "
+                f"{self.zip}, {self.deadline}, {self.weight}, {self.status}" + (f", {self.delivery_time}" if self.delivery_time else "") + ")")
